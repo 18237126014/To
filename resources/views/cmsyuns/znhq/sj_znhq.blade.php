263fd97ab87c1b4d41d8f50e624229b3997880f7
zwf <463390618@qq.com>
2018-03-20 14:59:00 +0800

    新增SJ

--- resources/views/cmsyuns/znhq/sj_znhq.blade.php
+++ resources/views/cmsyuns/znhq/sj_znhq.blade.php
@@ -4,8 +4,11 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>智能行情交易系统</title>
+		<link rel="stylesheet" href="{{url('/css/znhq/style.css')}}">
+		<link rel="stylesheet" href="{{url('/css/sj_form.css')}}">
 		<script src="{{url('/js/znhq/jquery-1.11.3.min.js')}}"></script>
 		<script src="{{url('/js/znhq/register.js')}}"></script>
+		<script src="{{url('/js/layer/layer.js')}}"></script>
 		<script>
 			(function(doc, win) {
 				var docEl = doc.documentElement,
@@ -39,8 +42,7 @@
 			})(document, window);
 		</script>
 
-		<link rel="stylesheet" href="{{url('/css/znhq/style.css')}}">
-		<link rel="stylesheet" href="{{url('/css/sj_form.css')}}">
+		
 		<script>
 			var _hmt = _hmt || [];
 			(function() {
@@ -56,16 +58,16 @@
 		<div class="wraptext">
 			<div class="wrap">
 				<p class="header">顶级平台 | 投资首选</p>
-				<img src="/images/znhq/jiaoyiImg-1.jpg" />
+				<img src="http://img.zengwf.com/images/znhq/jiaoyiImg-1.jpg" />
 				<div class="bannerDown">
 					<p>
 						<a href="javascript:void(0)">
-							<span  class="zsbtn-link p-link btn">免费下载</span>
+							<span  class="zsbtn-link p-link">免费下载</span>
 						</a>手机端正版软件
 					</p>
 				</div>
-				<img src="/images/znhq/jiaoyiImg-3.jpg" />
-				<img src="/images/znhq/jiaoyiImg-4.jpg" />
+				<img src="http://img.zengwf.com/images/znhq/jiaoyiImg-3.jpg" />
+				<img src="http://img.zengwf.com/images/znhq/jiaoyiImg-4.jpg" />
 				<div class="youshibg">
 					<h2 class="h3-title">
 					<span>软件优势</span>
@@ -99,8 +101,8 @@
 						</dl>
 					</div>
 				</div>
-				<img src="/images/znhq/jiaoyiImg-6.jpg" />
-				<img src="/images/znhq/jiaoyiImg-7.jpg" />
+				<img src="http://img.zengwf.com/images/znhq/jiaoyiImg-6.jpg" />
+				<img src="http://img.zengwf.com/images/znhq/jiaoyiImg-7.jpg" />
 				<div class="footer-text">
 					投资有风险，入市需谨慎
 				</div>
@@ -108,92 +110,57 @@
 					版权所有：金创互动科技（深圳）有限公司   ICP备案号：粤ICP备16102940号-1
 				</div>
 			</div>
-			<div class="down-fixed p-link btn">
+			<div class="down-fixed p-link">
 
 				<a href="javascript:void(0)">立即下载</a>
 			</div>
 		</div>
 {{--写跳转过来的页面--}}
 <input type="hidden" name="referr" value="<?= isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] :'' ?>">
-<!--遮罩层-->
-<div class="layui-layer-shade" id="layui-layer-shade3" time="3" style="display:none;"></div>
-<!--表单-->
-<div class="layui-layer layui-layer-page layer-form layer-anim" id="layui-layer3" type="page" showtime="0"time="3" contype="object" style="display: none; left:18rem;top:29rem">
-    <div id="" class="layui-layer-content">
-        <div class="modal-form layui-layer-wrap" id="modal-form" style="display: block;">
-            <p style="font-size: 40px;color:#41A5DD ;text-align: center;padding-top: 40px;">免费下载</p>
-            <p style="font-weight:normal; color:#999; font-family:宋体; font-size:14px;text-align: center;padding-top:10px;">（请正确填写手机号码，以便获取软件账号和密码！）</p>
-            <form id="form-1" class="validate nice-validator n-default" action="index1.html#" method="post" novalidate="novalidate">
+	<!--弹层-->
+		<div class="winboxbg" id="layui-layer-shade3" style='display:none;'>
+			<div class="winbox" id="layui-layer3">
+				<p class="p-close"></p>
+				<input type="hidden" name="_token" value="{{csrf_token()}}" id="_token">
+				<ul class="ul-listinput reg_div">
+					<li><input type="text" class="user_name" id='txtName1' placeholder="输入您的姓名"></li>
+					<li class="pb01"><input type="text" class="user_phone" id='txtPhone1' placeholder="输入您的手机号码">
 
-                <input type="hidden" name="_token" value="{{csrf_token()}}" id="_token">
-                <div class="form-content">
-                    <div class="form-group">
-                        <label for="txtName1" class="form-label">姓&nbsp;&nbsp;&nbsp;&nbsp;名</label>
-                        <div class="form-input">
-                            <input type="text" name="txtName1" id="txtName1" aria-required="true">
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                    <div class="form-group">
-                        <label for="txtPhone1" class="form-label">手机号</label>
-                        <div class="form-input">
-                            <input type="text" name="txtPhone1" id="txtPhone1" placeholder="" aria-required="true">
-                        </div>
-                        <div class="clear"></div>
-                    </div>
+					</li>
+					<li class="colorText">*非常重要，正确有效的手机号码才能成功开户</li>
+					<li class="pb03">
+						<div class="p-code">
+							<p class="pInput"> <input class="sms_code" id='txtSmsCode1' type="text" placeholder="输入验证码"></p>
+							<p class="code-btn"> <button class="sms_btn show_count" type="button" id='btnYzm1'>获取验证码</button></p>
+						</div>
 
+					</li>
+					<li> 
+						<input type='hidden' class='customer_description_item' value='赠金页面,第一个注册框'>
+						<button class="submit_btn" type="button" id="btn-submit" >免费下载</button>
+						<!-- <span class="form-group_button" id="btn-submit" type="button" onclick="">立即下载</span> -->
+					</li>
+				</ul>
+			</div>
+		</div>
+	</body>
 
+	<script>
+		$(function() {
+			$('.p-link').click(function() {
+				$(".winboxbg").show();
+				// $("#layui-layer-shade3").css('display','block');
+			});
+			$('.winboxbg .winbox .p-close').click(function() {
+				$(".winboxbg").hide();
+			});
+			$('.submit_btn').click(function(){
+				// $(".winboxbg").hide();
+				$(".winboxbg").css('display','none');
+			});
 
-                    <div class="form-group">
-                        <label for="txtSmsCode1" class="form-label">验证码</label>
-                        <div class="form-input">
-                            <input type="text" name="txtSmsCode1" id="txtSmsCode1" class="input-vcode" maxlength="6">
+		});
+	</script>
+	<script src="{{url('js/form.js')}}"></script>
 
-                            <button id="btnYzm1" class="dz_yzm" value="免费获取验证码" onclick="javascript:return false;">
-                                <span id="SendTxt1">免费获取验证码 </span>
-                            </button>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-
-                    <div class="form-group" style="height: 70px">
-                        <span class="form-group_button " id="btn-submit" type="button" onclick="">立即下载</span>
-
-                    </div>
-                    <div class="form-footer-text">
-                        <p>我们采用加密技术确保您的信息安全，您的联系信息仅用于发送技术指标，请放心提交</p>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-    <span class="layui-layer-setwin"><a class="layui-layer-ico layui-layer-close layui-layer-close2" href="javascript:void (0);"></a></span>
-    <span class="layui-layer-resize"></span>
-</div>
-<!--微信二维码-->
-<div class="weixin" style="display: none">
-    <div class="wxewm">
-        <img src="http://img.zengwf.com/images/wxh/01.jpg" alt="" width="200">
-    </div>
-    <p class="tishi">如安装过程中遇到问题，请扫描二维码添加您的专属客服或添加微信号<span style="color:red">HX201805</span ></p>
-    <p style="margin-top: 1rem;font-size: 2rem;text-align: center;" class="weix_colse">
-        <a href="javascript:void(0)" class="wx_colse">关闭</a>
-    </p>
-</div>
-<script>
-</script>
-<script>
-    //获取下载按钮
-    var Btns = $('.btn');
-    //获取遮罩层和表单元素
-    var mask = $('#layui-layer-shade3');
-    var Form = $('#layui-layer3');
-    $(Btns).click(function()
-    {
-        $(mask).css('display','block');
-        $(Form).css('display','block');
-    });
-</script>
-<script src="{{url('js/form.js')}}"></script>
-</body>
 </html>
